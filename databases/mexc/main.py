2e6de76d500c93fdca9bf931c5bad576d41b62c3
Jardinesdelareina <jardinesdelareina@mail.ru>
2024-04-02 22:36:17 +0300

    Подключение в python

--- databases/mexc/main.py
+++ databases/mexc/main.py
@@ -1,7 +1,6 @@
 import requests
 import environs
-import pandas as pd
-from sqlalchemy import create_engine
+import psycopg2
 
 env = environs.Env()
 env.read_env('.env')
@@ -9,32 +8,34 @@ env.read_env('.env')
 DB_USER = env('DB_USER')
 DB_PASS = env('DB_PASS')
 DB_HOST = env('DB_HOST')
-DB_PORT = env('DB_PORT')
 DB_NAME = env('DB_NAME')
-ENGINE = create_engine(
-    f'postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
-)
 
-
-def main(symbol: str, interval: str = 'Min15'):
-    res = requests.get(
-        f'https://contract.mexc.com/api/v1/contract/kline/{symbol}?interval={interval}'
+try:
+    # Устанавливаем соединение с базой данных
+    connection = psycopg2.connect(
+        host=DB_HOST,
+        database=DB_NAME,
+        user=DB_USER,
+        password=DB_PASS
     )
-    res_time = res.json()['data']['time']
-    res_open = res.json()['data']['open']
-    res_high = res.json()['data']['high']
-    res_low = res.json()['data']['low']
-    res_close = res.json()['data']['close']
-    data = {
-        'm_time': res_time,
-        'm_open': res_open,
-        'm_high': res_high,
-        'm_low': res_low,
-        'm_close': res_close,
-    }
-    df = pd.DataFrame(data)
-    df = df.astype(float)
-    df.to_sql(name=f'{symbol.replace("_", "").lower()}', con=ENGINE, if_exists='append', index=False)
-    print('[SUCCESS]')
 
-main('XRP_USDT')
+    # Создаем курсор для выполнения SQL-запросов
+    cursor = connection.cursor()
+
+    # Выполняем SQL-запрос
+    cursor.execute("SELECT version();")
+
+    # Получаем результат выполнения запроса
+    db_version = cursor.fetchone()
+    print("PostgreSQL database version:", db_version)
+
+    # Закрываем курсор
+    cursor.close()
+
+except (Exception, psycopg2.Error) as error:
+    print("Ошибка подключения к базе данных:", error)
+
+finally:
+    # Закрываем соединение с базой данных
+    if connection:
+        connection.close()
